fd7e9ba022e74fa8e2496e7c124924255606ae03
JaviSrz <ljsuarez7@gmail.com>
2025-04-19 10:09:11 +0200

    Creación de rutas y menu para navegar entre ellas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes básicos',
+    loadComponent: () => import('./pages/basic-page/basic-page.component'),
+  },
+  {
+    path: 'numbers',
+    title: 'Pipes de numeros',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page.component'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic'
+  }
+];
