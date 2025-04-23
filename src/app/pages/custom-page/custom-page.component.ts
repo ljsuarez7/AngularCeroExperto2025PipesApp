b5a2224b532bd6708cbdfb28dd8d94bbc0004927
JaviSrz <ljsuarez7@gmail.com>
2025-04-23 20:14:56 +0200

    Pipe para cambiar el color del texto de la columan color

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -4,10 +4,12 @@ import { Hero } from '../../interfaces/hero.interface';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
